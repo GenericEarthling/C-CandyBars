b8e1b728715bda34a3211f4b066ce1eac0152a14
GenericEarthling <sharon.tender@icloud.com>
2017-07-01 16:58:49 -0400

    Fixed calorieArray displaying wrong values

--- CandyBars/main.c
+++ CandyBars/main.c
@@ -15,10 +15,11 @@
 #include <ctype.h>
 #define LBS_TO_KG 0.45
 
-// function prototype
+// Function prototype
 int caloriesCalc(int, float, float);
 
-// function that returns a value
+// Function that Calculates the metabolic rate. And returns the minutes
+// as an integer. Formula found online but I don't remember where.
 int calorieCalc(int caloriesInteger, float metFloat, float weightInKgFloat) {
     int minutesInteger;
     minutesInteger = (caloriesInteger * 200) / (metFloat * 3.5 * weightInKgFloat);
@@ -26,49 +27,49 @@ int calorieCalc(int caloriesInteger, float metFloat, float weightInKgFloat) {
 }
 
 int main() {
-    // variable declaration
+    // Variable declaration
     int weight, counter=0, calories, caloriesArray[counter], items;
     int numMinutes;
     float weightInKg, met;
     char selection;
 
-    // Describe the program to the user
-    // prompt user to enter number of sweet foods to calculate for
+    // Describes the program to the user, then prompts user to enter
+    // number of snack items they want to calculate for exercise time
     printf("\n  -------------- BURNING CALORIES ---------------");
     printf("\n  This program will calculate the number ");
     printf("\n  of minutes you need to exercise in ");
     printf("\n  order to burn off the calories in ");
-    printf("\n  a candy bar or dessert.");
+    printf("\n  a candy bar, snack, or dessert.");
     printf("\n  -----------------------------------------------\n");
     printf("\n  Enter the number of sweets you would ");
     printf("\n  like to calculate the values for --> ");
     scanf("%d", &items);
 
-    // loop through number of sweets user specified above to prompt for
+    // Loop through number of sweets user specified above to prompt for
     // the number of calories in each one. Store results into an array.
     for (counter=0; counter < items; counter++) {
         printf("\n  Enter the number of calories sweet #%d has --> ", counter+1);
         scanf("%d", &caloriesArray[counter]);
     }
 
-    // prompt user for weight to determine which metabolic rate to use
-    // in calculating exercise routine.
+    // Prompt user for their weight to determine which metabolic rate to use
+    // in calculating exercise routine time.
     printf("\n  Enter your weight: ");
     scanf("%d", &weight);
 
-    // Error handling for invalid user input
+    // Error handling for out of range user input
     if ((weight >=100) && (weight<=300)) {
-            weightInKg = weight * LBS_TO_KG;
+        weightInKg = weight * LBS_TO_KG;
     } else {
-            do {
-                    printf("\n  Your weight is out of range. Please enter a weight ");
-                    printf("\n  that is between 100 to 300lbs: ");
-                    scanf("%d", &weight);
-                    weightInKg = weight * LBS_TO_KG;
-            } while (weight < 100 || weight > 300);
+        do {
+            printf("\n  Your weight is out of range. Please enter a weight ");
+            printf("\n  that is between 100 to 300lbs: ");
+            scanf("%d", &weight);
+            weightInKg = weight * LBS_TO_KG;
+        } while (weight < 100 || weight > 300);
     }
 
-    // Prompt use for selecting an activity to participate in for the switch stmt.
+    // Prompt user for selecting an activity for the switch stmt.
     printf("\n  Which activity would you rather participate in?");
     printf("\n  Please enter the letter next to activity. ");
     printf("\n  A) Running - 4 mph");
@@ -100,16 +101,18 @@ int main() {
                     met = 8.0;
                     break;
     }
-
-    // loop through each sweet to display the number of minutes user would 
-    // have to exercise to burn off the calories
+    
+    // Display results.
+    // Loop through each sweet to display the number of minutes user would 
+    // have to exercise to burn off the calories in the sweet item.
     counter = 0;
     while (counter < items) {
-            calories = caloriesArray[counter];
-            numMinutes = calorieCalc(calories, met, weightInKg);
-            printf("\n  Sweet #%d has %d calories. You will ", counter +1, calories);
-            printf("\n  need to workout for %d minutes.\n", numMinutes);
-            counter++;
+        calories = caloriesArray[counter];
+        numMinutes = calorieCalc(calories, met, weightInKg);
+        printf("\n  Sweet #%d ", counter+1);
+        printf("has %d calories. You will ", calories);
+        printf("\n  need to workout for %d minutes.\n", numMinutes);
+        counter++;
     }
     printf("\n  ------------------- end ------------------------\n");
 
